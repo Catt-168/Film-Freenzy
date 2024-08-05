66bd13e90690c0941b362a933993a6fc54dee47f
Catt-168 <satpaingthu2000@gmail.com>
2024-08-05 23:30:45 +0630

    feat: added bar chart

--- frontend3/src/components/Admin/Dashboard/Dashboard.jsx
+++ frontend3/src/components/Admin/Dashboard/Dashboard.jsx
@@ -1,19 +1,22 @@
-import * as React from "react";
+import { Box, Slider, Typography } from "@mui/material";
 import { BarChart } from "@mui/x-charts/BarChart";
 import { PieChart } from "@mui/x-charts/PieChart";
-import { Box } from "@mui/material";
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { SERVER } from "../../../constants";
+import { Colors } from "../../../helpers/constants";
+import restClient from "../../../helpers/restClient";
 import useAuth from "../../hooks/useAuth";
 import AdminNavigation from "../../Navigation/AdminNavigation";
-import { useEffect } from "react";
-import restClient from "../../../helpers/restClient";
-import { SERVER } from "../../../constants";
-import { useState } from "react";
-import { Colors } from "../../../helpers/constants";
+
+const PIE_SLIDER_MIN = 3;
 
 export default function Dashboard() {
   const { user, isAdmin } = useAuth();
 
   const [pieData, setPieData] = useState([]);
+  const [pieDataCount, setPieDataCount] = useState(3);
+  const [barChartData, setBarchData] = useState([]);
 
   async function fetchPieData() {
     const response = await restClient.get(
@@ -22,27 +25,120 @@ export default function Dashboard() {
     setPieData(response.data.pieData);
   }
 
+  async function fetchBarChartData() {
+    const response = await restClient.get(
+      `${SERVER}/dashboard/getBarChartData`
+    );
+    setBarchData(response.data.barChartData);
+  }
+
   useEffect(() => {
     fetchPieData();
+    fetchBarChartData();
   }, []);
 
-  const sortedPieData = pieData?.sort((a, b) => b.value - a.value).slice(0, 6);
-  console.log(sortedPieData);
+  const moviesData = [
+    { movieId: 1, title: "Movie A", rentalCount: 100 },
+    { movieId: 2, title: "Movie B", rentalCount: 80 },
+    // ... more movies
+  ];
+  const chartData = {
+    columns: [
+      { field: "title", headerName: "Movie Title", width: 200 },
+      { field: "rentalCount", headerName: "Rental Count", type: "number" },
+    ],
+    rows: moviesData,
+  };
+  const sortedPieData = pieData
+    ?.sort((a, b) => b.value - a.value)
+    .slice(0, pieDataCount);
+
+  const barChartX = [];
+  const barChartY = [];
+  barChartData.forEach((b) => {
+    barChartX.push(b[0]);
+    barChartY.push(b[1]);
+  });
+  console.log(pieDataCount);
+
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: "flex", height: "100vh" }}>
       <AdminNavigation />
-      <Box sx={{ padding: "2rem", mt: 5 }}>
-        <PieChart
-          colors={colors}
-          series={[
-            {
-              data: sortedPieData,
-            },
-          ]}
-          width={600}
-          height={400}
-        />
-        <h1>Top Movies by Genre</h1>
+      <Box
+        sx={{
+          mt: 15,
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          gap: 5,
+          width: "70%",
+        }}
+      >
+        <Box
+          sx={{
+            padding: "1rem",
+            ml: 5,
+            border: "1px solid #D3D3D3",
+            borderRadius: 2,
+            maxHeight: 500,
+          }}
+        >
+          <Typography id="input-item-number" gutterBottom>
+            Number of items
+          </Typography>
+          <Slider
+            value={pieDataCount}
+            onChange={(e, newValue) => setPieDataCount(newValue)}
+            valueLabelDisplay="auto"
+            min={PIE_SLIDER_MIN}
+            max={pieData.length}
+            aria-labelledby="input-item-number"
+          />
+          <PieChart
+            colors={colors}
+            series={[
+              {
+                data: sortedPieData,
+                highlightScope: { faded: "global", highlighted: "item" },
+                faded: {
+                  innerRadius: 30,
+                  additionalRadius: -30,
+                  color: "gray",
+                },
+              },
+            ]}
+            width={800}
+            height={300}
+          />
+          <Typography fontSize={24} color={Colors.primary}>
+            Top Movies by Genre
+          </Typography>
+        </Box>
+
+        <Box
+          sx={{
+            padding: "1rem",
+            ml: 5,
+            border: "1px solid #D3D3D3",
+            borderRadius: 2,
+            maxHeight: 400,
+          }}
+        >
+          <BarChart
+            xAxis={[{ scaleType: "band", data: barChartX }]}
+            series={[
+              {
+                data: barChartY,
+              },
+            ]}
+            width={900}
+            height={300}
+            colors={colors}
+          />
+          <Typography fontSize={18} color={Colors.primary}>
+            Top Purchased Movies
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
