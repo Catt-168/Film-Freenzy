0194540bb3501b7eea14b06905aee09c3513e78f
thinkabyaroo <thinkabyar@isgm2.com>
2024-08-17 18:03:48 +0630

    Modify the purchase list

--- frontend3/src/components/TableBody/TableBody.jsx
+++ frontend3/src/components/TableBody/TableBody.jsx
@@ -268,12 +268,12 @@ function AdminRentalsTableBody({ items }) {
           <TableCell size="large" align="left">
             <Link to={`/admin/movies/${row.movie._id}`}>{row.movie.title}</Link>
           </TableCell>
-          <TableCell>
+          {/* <TableCell>
             {generateExpireDate(row.dateOut, row.rentalDate)}
-          </TableCell>
+          </TableCell> */}
           <TableCell>{row.rentalFee}</TableCell>
           <TableCell>{new Date(row.dateOut).toDateString()}</TableCell>
-          <TableCell>{row.movie.price}</TableCell>
+          {/* <TableCell>{row.movie.price}</TableCell> */}
         </TableRow>
       ))}
     </TableBody>
